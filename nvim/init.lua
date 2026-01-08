34bd101aef028559afc135106826592c810a4d4e
jackson-lafrance <jackson@codeflight.dev>
2026-01-08 00:37:20 -0500

    formatting yay

--- nvim/init.lua
+++ nvim/init.lua
@@ -115,10 +115,10 @@ map({ 'n' }, '<leader>sd', builtin.diagnostics, { desc = 'Diagnostics' })
 
 --- LSP and Completion ---
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  vim.lsp.config('lua_ls', {
-    capabilities = capabilities
-  })
-  vim.lsp.enable('lua_ls')
+vim.lsp.config('lua_ls', {
+  capabilities = capabilities
+})
+vim.lsp.enable('lua_ls')
 
 map({ 'n' }, '<leader>lf', vim.lsp.buf.format, { desc = 'Format current buffer' })
 map({ 'n' }, '<leader>bb', function()
@@ -145,24 +145,24 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
     ["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, {"i", "s"}),
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {"i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
