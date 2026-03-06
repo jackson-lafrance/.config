cf8e2dc4208ae2fc16ed21a35d251583382054ad
Jackson Lafrance <jackson.lafrance@shopify.com>
2026-03-06 11:49:19 -0500

    snippet removal

--- nvim/snippets/javascriptreact.lua
+++
@@ -1,3 +0,0 @@
--- React / React Native snippets (JSX) - inherits from TSX
--- This file loads the TSX snippets since they work the same
-return require("luasnip.loaders.from_lua").load_file(vim.fn.stdpath("config") .. "/snippets/typescriptreact.lua") or {}
