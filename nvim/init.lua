fb05fe32c6e00a00a521a8e41a68e216a4df88b3
Jackson Lafrance <jackson.lafrance@shopify.com>
2026-03-09 11:13:20 -0400

    pasting clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -368,6 +368,7 @@ map({ 'n', 'v', 'x' }, '<leader>r', ":restart<CR>", { desc = "Restart vim" })
 
 map('', '<leader>y', '"+y', { desc = 'Copy to clipboard' })
 map('', '<leader>d', '"+d', { desc = 'Cut to clipboard' })
+map('', '<leader>pc', '"+p', { desc = 'Paste clipboard' })
 map('', '<leader>pp', '"0p', { desc = 'Paste last yanked' })
 map('', '<leader>pd', '"1p', { desc = 'Paste last deleted' })
 
