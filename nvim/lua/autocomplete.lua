8071ee0aefa7bb9d2aabc8dd328ba697bbb1a7fe
Jackson Lafrance <jackson@codeflight.dev>
2026-05-15 00:56:57 -0400

    completion keybinds

--- nvim/lua/autocomplete.lua
+++ nvim/lua/autocomplete.lua
@@ -12,3 +12,27 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end,
 })
+
+local function pum()
+  return vim.fn.pumvisible() == 1
+end
+
+vim.keymap.set("i", "<C-l>", function()
+  vim.lsp.completion.get()
+end, { desc = "trigger completion" })
+
+vim.keymap.set("i", "<CR>", function()
+  return pum() and vim.fn.complete_info({ "selected" }).selected ~= 1 and "<C-y>" or "<CR>"
+end, { expr = true, desc = "accept completion" })
+
+vim.keymap.set("i", "<Tab>", function()
+  return pum() and "<C-n>" or "<Tab>"
+end, { expr = true, desc = "next completion item" })
+
+vim.keymap.set("i", "<S-Tab>", function()
+  return pum() and "<C-p>" or "<S-Tab>"
+end, { expr = true, desc = "previous completion item" })
+
+vim.keymap.set("i", "<C-c>", function()
+  return pum() and "<C-e>" or "<C-c>"
+end, { expr = true, desc = "cancel completion" })
